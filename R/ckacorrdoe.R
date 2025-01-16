3ed2410f8fd2ebf9dde94ca85f4f7be0107296e3
Premchintu33 <premchintu33@yahoo.com>
2025-01-16 23:11:58 +0530

    with new P matrix, treatment combinations code

--- R/ckacorrdoe.R
+++ R/ckacorrdoe.R
@@ -69,8 +69,33 @@ ckacorrdoe <- function(trt,Rep,resp,noutli){
   ## labeling
   n <- length(trt)
   I_mat <- diag(n)
+  r <- length(unique(Rep)) ## number of replications
   v <- length(unique(trt)) ## number of treatments
   j.vec <-  matrix(1, nrow = n, ncol = 1)
+
+  ##generating P matrix
+  generate_contrast_matrix <- function(v) {
+    if (v < 2) {
+      stop("The number of treatments (v) must be at least 2.")
+    }
+
+    # Create an empty matrix for (v-1) x v
+    P <- matrix(0, nrow = v - 1, ncol = v)
+
+    # Fill the matrix using Helmert contrasts
+    for (i in 1:(v - 1)) {
+      P[i, 1:i] <- 1
+      P[i, i + 1] <- -i
+    }
+
+    # Normalize each row (optional)
+    P <- t(apply(P, 1, function(row) row / sqrt(sum(row^2))))
+
+    return(P)
+  }
+
+  ####
+  P <- generate_contrast_matrix(v)
 
   ##design matrices
   des_trr <- unname(model.matrix(~0+as.factor(trt)))
@@ -123,7 +148,6 @@ ckacorrdoe <- function(trt,Rep,resp,noutli){
   Hstr <- inv(omg_fl) %*% Vmat_ac
 
   ## for cook distance
-  ckd_acmat <- matrix(0,nrow =n, ncol = 1)
 
   # Function to generate all possible outlier vectors with specified number of outliers
   generate_outvec <- function(n, num_outliers) {
@@ -148,16 +172,55 @@ ckacorrdoe <- function(trt,Rep,resp,noutli){
 
   u_lis <- generate_outvec(n,noutli)
 
+  ckd_acmat <- matrix(0,nrow =length(u_lis), ncol = 1)
+
   ## loop for cook's distance
-  for(outl in 1:n){
-    omg_str_nu <- t(as.vector(u_lis[[outl]])) %*% inv(omg_fl) %*% Vmat_ac %*% resp
-    omg_str_de <- sd(resp) * sqrt(t(as.vector(u_lis[[outl]])) %*% Vmat_ac %*% as.vector(u_lis[[outl]]))
-    omg_str <- omg_str_nu/omg_str_de
-    ckd_ac_fr <- (bmat_ac[outl,outl] - Hstr[outl,outl])/Hstr[outl,outl]
-    ckd_ac_sc <- (omg_str^2)/(v-1)
-    ckd_ac <- ckd_ac_fr * ckd_ac_sc
+  for(outl in 1:length(u_lis)){
+    uVu_ac <- t(as.vector(u_lis[[outl]])) %*% inv(omg_fl) %*% Vmat_ac %*% as.vector(u_lis[[outl]])
+    P_thdiff <- (P %*% pinv(cmat_ac) %*% t(des_trr) %*% bmat_ac %*% as.vector(u_lis[[outl]]) %*% inv(uVu_ac) %*%
+                   t(as.vector(u_lis[[outl]])) %*% inv(omg_fl) %*% Vmat_ac %*% resp)
+    pcp_ac <- P %*% cmat_ac %*% t(P)
+    ckd_ac_num <- t(P_thdiff) %*% pcp_ac %*% P_thdiff
+    ckd_ac_deno <- var(resp) * (v-1)
+    ckd_ac <- ckd_ac_num/ckd_ac_deno
     ckd_acmat[outl,] <- ckd_ac
   }
+
+  # Function to explore all possible combinations of deletions
+  explore_combinations <- function(data, delete_count) {
+    # Get all non-NA indices
+    non_na_indices <- which(!is.na(data), arr.ind = TRUE)
+
+    # Generate all combinations of the specified size
+    combinations <- combn(seq_len(nrow(non_na_indices)), delete_count)
+
+    # Store the combinations explored
+    explored <- list()
+
+    # Iterate through each combination
+    for (i in seq_len(ncol(combinations))) {
+      # Get the indices for this combination
+      combo_indices <- non_na_indices[combinations[, i], , drop = FALSE]
+
+      # Save this combination
+      explored[[i]] <- combo_indices
+    }
+    trt_combs <- data.frame(0,nrow = length(explored), ncol = 1)
+    for (i in seq_along(explored)) {
+      itrt_comb <-  data.frame(0,nrow =1 , ncol = delete_count)
+      for (j in seq_len(nrow(explored[[i]]))) {
+        itrt_comb[,j] <- paste0("(", explored[[i]][j, "row"], ", ", explored[[i]][j, "col"], ") ")
+      }
+      trt_combs[i,] <- itrt_comb
+    }
+
+    return(trt_combs[,1:delete_count])
+  }
+  ### data for trt comb
+  data_fmatac <- xtabs(resp~trt+Rep)
+
+  tr_combac <- explore_combinations(matrix(data_fmatac, nrow = v,ncol = r), delete_count = noutli)
+
   ## to mark values with threshold values
   ## function to mark
   mark_values_with_asterisk <- function(mat, threshold) {
@@ -177,7 +240,14 @@ ckacorrdoe <- function(trt,Rep,resp,noutli){
   fthdac <- IQR(ckd_acmat)
   trshld_ac <- (7/2) * fthdac
   ckd_acmatt <- mark_values_with_asterisk(ckd_acmat,trshld_ac)
+
+  tr_comb2ac <- as.data.frame(tr_combac)
+  for (i in seq_along(tr_comb2ac)) {
+    colnames(tr_comb2ac)[i] <- paste("(trt_rep)", i, sep = "_")
+  }
+  ckd_acdf2 <- cbind.data.frame(tr_comb2ac,ckd_acmatt)
+
   cat('------------------------------------------------------------------------
       \nCook s Distance : \n')
-  print(ckd_acmatt)
+  print(ckd_acdf2)
 }
